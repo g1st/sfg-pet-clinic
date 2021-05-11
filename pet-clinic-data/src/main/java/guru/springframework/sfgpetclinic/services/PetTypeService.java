47cfe52aada0740db5a5436d4e228d9ec6f9cd7e
Gintaras Stankus <16963832+g1st@users.noreply.github.com>
2021-05-11 11:14:57 +0100

    Add PetType service

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+
+/**
+ * Created by gs on 11/05/2021.
+ **/
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
